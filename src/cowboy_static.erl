9191e99b1f7e07fb6a3012e58a8755137a0670c6
Magnus Klaar <magnus.klaar@gmail.com>
2011-11-08 20:11:01 +0100

    remove unused integer_to_binary function

--- src/cowboy_static.erl
+++ src/cowboy_static.erl
@@ -377,11 +377,6 @@ esc_segment(<<C, Rest/binary>>, Acc) ->
 esc_segment(<<>>, Acc) ->
     Acc.
 
-%% @private Convert an integer to a binary string.
--spec integer_to_binary(integer()) -> binary().
-integer_to_binary(Int) ->
-    list_to_binary(integer_to_list(Int)).
-
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
